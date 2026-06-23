c43da82c7deedb5fb28a73993b9e28fdd1a9c231
Manu <manuel.lindinger@stud.edubs.ch>
2026-06-23 22:52:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+async function setdetails() {
+    const container = document.querySelector('.projects');
+    const containerinfo = document.querySelector('.projects-info');
+
+    try {
+        const response = await fetch('projekte.json');
+        const projects = await response.json();
+
+        if (projects.length === 0) {
+            container.innerHTML = 'Keine Projekte gefunden';
+            return;
+        }
+
+        containerinfo.innerHTML = `<p id="project-info"><b>Anzahl Projekte: </b>${projects.length}</p>`;
+        projects.forEach(project => {
+            container.innerHTML += `
+                <div class="project">
+                    <div>
+                        <h2 class="project-title">${project.title}</h2>
+                        <p class="project-description hidden">
+                            ${project.description}
+                        </p>
+                    </div>
+
+                    <button class="dropdown-btn">﹀</button>
+                </div>
+            `;
+        });
+        document.querySelectorAll('.dropdown-btn').forEach(button => {
+            button.addEventListener('click', () => {
+
+                const description =
+                    button.parentElement.querySelector('.project-description');
+
+                description.classList.toggle('hidden');
+
+                button.textContent =
+                    description.classList.contains('hidden') ? '﹀' : '︿';
+            });
+        });
+
+    } catch (error) {
+        container.innerHTML = '<p class="project-text" id="project-error">JSON nicht verfügbar</p>';
+        console.error(error);
+    }
+}
+
+setdetails();
